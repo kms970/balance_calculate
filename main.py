07a802e512de3e9d0c592f8503ea521042e850fa
kms970 <49391285+kms970@users.noreply.github.com>
2021-04-28 19:03:46 +0900

    RequestOderList(allList)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import connectDB
 import pandas as pd
+import RequestOrderList
 
 def print_hi(name):
     # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
@@ -9,4 +10,5 @@ def print_hi(name):
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
     mainDataFrame = pd.DataFrame(connectDB.MyDataBase().ConnectDataBase())
-    print(mainDataFrame)
+    #print(mainDataFrame)
+    #print(RequestOrderList.body)
