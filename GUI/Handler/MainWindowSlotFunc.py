8a4f5815222f549bb7403871bcfd04660a27b9a9
김민석 <kkhan97@naver.com>
2021-06-01 18:46:30 +0900

    날짜 값 전달(핸들러 이용)

--- GUI/Handler/MainWindowSlotFunc.py
+++ GUI/Handler/MainWindowSlotFunc.py
@@ -9,6 +9,7 @@ class MainWindowFunc(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MainWindowFunc, self).__init__(parent)
         self.setupUi(self)
         self.calculate_window = CalculateDialogFunc(self)
+        self.at_to.dateChanged.connect(self.is_date_true)
         self.do_calculate.clicked.connect(self.do_calculate_button)
 
     def do_calculate_button(self):
@@ -16,7 +17,7 @@ class MainWindowFunc(QtWidgets.QMainWindow, Ui_MainWindow):
             msgBox = QtWidgets.QMessageBox()
             msgBox.setWindowTitle("날짜오류")
             msgBox.setText("날짜를 조정해주세요")
-            self.at_from.setDate(QDate.currentDate())
+            self.at_from.setDate(self.at_to.date())
             msgBox.exec_()
 
         else:
@@ -26,6 +27,9 @@ class MainWindowFunc(QtWidgets.QMainWindow, Ui_MainWindow):
             #self.calculate_window.CalculateLabel.
             self.calculate_window.show()
 
+    def is_date_true(self):
+        if self.at_to.date().daysTo(QDate.currentDate()) < 0:
+            self.at_to.setDate(QDate.currentDate())
 
 class CalculateDialogFunc(QtWidgets.QDialog, Ui_CalculateDialog):
     def __init__(self, parent=None):
